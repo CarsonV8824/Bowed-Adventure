6211c64ded12d7ab74bbbd20ac5c1a779bad1026
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-07-15 20:56:20 -0500

    TODO: add in the two screens

---
+++ screens/display_piece_screen.cs
@@ -0,0 +1,22 @@
+using Raylib_cs;
+
+class DisplayScreen : Screen
+{
+    public int PlayerResult {get;set;}
+    public int ComputerResult {get;set;}
+
+    public DisplayScreen()
+    {
+        
+    }
+
+    public override void Draw()
+    {
+        
+    }
+
+    public override void Update(float dt)
+    {
+        
+    }
+}
---
+++ screens/result_screen.cs
@@ -0,0 +1,22 @@
+using Raylib_cs;
+
+class ResultScreen : Screen
+{
+    public int PlayerResult {get;set;}
+    public int ComputerResult {get;set;}
+
+    public ResultScreen()
+    {
+        
+    }
+
+    public override void Draw()
+    {
+        
+    }
+
+    public override void Update(float dt)
+    {
+        
+    }
+}
